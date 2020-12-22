cc4b55886dc60307c02b161a71cf7c5c5a98a51e
TomoShishido <shishid@ad.cyberhome.ne.jp>
2020-12-22 22:24:13 +0900

    Initial Commit

---
+++ testForFileManager/testForFileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testForFileManagerApp.swift
+//  testForFileManager
+//
+//  Created by 宍戸知行 on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct testForFileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
